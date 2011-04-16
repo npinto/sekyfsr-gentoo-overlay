5b6af60731beca466e06a69d4420b5b04eae4aef
Nicolas Pinto <pinto@mit.edu>
2011-04-15 23:43:49 -0400

    Add torque ebuild's missing files

---
+++ sys-cluster/torque/files/torque-env.d
@@ -0,0 +1,7 @@
+# Copyright 1999-2005 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License, v2 or later
+
+# Configuration files that are included in the initial Torque install
+PBS_SERVER_HOME="/var/spool/torque"
+CONFIG_PROTECT="/var/spool/torque"
+
